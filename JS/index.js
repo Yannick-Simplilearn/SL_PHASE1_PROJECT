08fbf3e75b2edefac7d470eaebb61e1bf90992c5
Yannick Vollmer <yannick.simplilearn@gmx.de>
2020-12-03 14:39:54 +0100

    created login page with css and js-validation

--- JS/index.js
+++
@@ -1,7 +0,0 @@
-//navbar dropdown
-const burger = document.getElementsByClassName('burger')[0];
-const navbar = document.getElementsByClassName('navbar-elements')[0];
-
-burger.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-})
---
+++ JS/script.js
@@ -0,0 +1,24 @@
+//navbar dropdown
+const burger = document.getElementsByClassName('burger')[0];
+const navbar = document.getElementsByClassName('navbar-elements')[0];
+
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+})
+
+//login check
+function checkInput() {
+    let uname = document.getElementById("uname").value;
+    let pw = document.getElementById("pw").value;
+
+    if(uname.length == 0 && pw.length == 0) {
+        document.getElementById("validate").innerHTML = "Username and Password is required";
+        return false;
+    }else if(pw.length == 0){
+        document.getElementById("validate").innerHTML = "Password is required";
+        return false;
+    }else if(uname.length == 0){
+        document.getElementById("validate").innerHTML = "Username is required";
+        return false;
+    }
+}
